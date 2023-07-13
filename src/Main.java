08ae9cf594df3544dc9347625f9778575def9e38
Pedro Araujo <araujosantillanp8@gmail.com>
2023-07-13 01:31:15 -0500

    Refactorizacion de codigo realizada

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 import java.util.Arrays;
 
 public class Main {
-
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    // Crear una instancia de RentalInfo
+    RentalInfo rentalInfo = new RentalInfo();
 
+    // Crear una instancia de Customer con los alquileres correspondientes
+    Customer customer = new Customer("C. U. Stomer", Arrays.asList(
+            new MovieRental("F001", 3),
+            new MovieRental("F002", 1)
+    ));
+
+    // Crear una instancia de RentalCalculator dentro de RentalInfo
+    RentalInfo.RentalCalculator rentalCalculator = rentalInfo.new RentalCalculator();
+
+    // Calcular la declaración de alquiler utilizando RentalCalculator
+    String result = rentalCalculator.calculateStatement(customer);
+
+    // Comparar el resultado con el valor esperado
     if (!result.equals(expected)) {
       throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
     }
--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -2,49 +2,79 @@ import java.util.HashMap;
 
 public class RentalInfo {
 
-  public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+  public class RentalCalculator {
+    private HashMap<String, Movie> movies; // Mapa para almacenar las películas
+    public RentalCalculator() {
+      movies = new HashMap<>(); // Inicialización del mapa de películas
 
-    double totalAmount = 0;
-    int frequentEnterPoints = 0;
-    String result = "Rental Record for " + customer.getName() + "\n";
-    for (MovieRental r : customer.getRentals()) {
+      // Agregar películas al mapa con sus respectivos códigos
+      movies.put("F001", new Movie("You've Got Mail", "regular"));
+      movies.put("F002", new Movie("Matrix", "regular"));
+      movies.put("F003", new Movie("Cars", "childrens"));
+      movies.put("F004", new Movie("Fast & Furious X", "new"));
+    }
+
+    // Método para calcular la declaración de alquiler
+    public String calculateStatement(Customer customer) {
+      double totalAmount = 0; // Variable para almacenar el monto total
+      int frequentEnterPoints = 0; // Variable para almacenar los puntos de cliente frecuente
+      StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n"); // StringBuilder para construir la declaración
+
+      for (MovieRental rental : customer.getRentals()) {
+        double thisAmount = calculateAmount(rental); // Calcular el monto para cada alquiler
+
+        frequentEnterPoints++; // Incrementar los puntos de cliente frecuente
+
+        // Agregar puntos de cliente frecuente adicionales para un alquiler de nueva versión de dos días
+        if (movies.get(rental.getMovieId()).getCode().equals("new") && rental.getDays() > 2) {
+          frequentEnterPoints++;
+        }
+
+        // Agregar detalles del alquiler a la declaración
+        result.append("\t")
+                .append(movies.get(rental.getMovieId()).getTitle())
+                .append("\t")
+                .append(thisAmount)
+                .append("\n");
+
+        totalAmount += thisAmount; // Acumular el monto total
+      }
+
+      // Agregar líneas de pie de página
+      result.append("Amount owed is ")
+              .append(totalAmount)
+              .append("\n");
+      result.append("You earned ")
+              .append(frequentEnterPoints)
+              .append(" frequent points\n");
+
+      return result.toString(); // Devolver la declaración como una cadena de texto
+    }
+
+    // Método para calcular el monto de alquiler para una película específica
+    private double calculateAmount(MovieRental rental) {
       double thisAmount = 0;
 
-      // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
+      // Obtener la película correspondiente al ID del alquiler
+      Movie movie = movies.get(rental.getMovieId());
+
+      // Calcular el monto según el tipo de película
+      if (movie.getCode().equals("regular")) {
         thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
+        if (rental.getDays() > 2) {
+          thisAmount += (rental.getDays() - 2) * 1.5;
         }
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
+      } else if (movie.getCode().equals("new")) {
+        thisAmount = rental.getDays() * 3;
+      } else if (movie.getCode().equals("childrens")) {
         thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
+        if (rental.getDays() > 3) {
+          thisAmount += (rental.getDays() - 3) * 1.5;
         }
       }
 
-      //add frequent bonus points
-      frequentEnterPoints++;
-      // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
-
-      //print figures for this rental
-      result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
-      totalAmount = totalAmount + thisAmount;
+      return thisAmount; // Devolver el monto de alquiler calculado
     }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentEnterPoints + " frequent points\n";
-
-    return result;
   }
 }
+
